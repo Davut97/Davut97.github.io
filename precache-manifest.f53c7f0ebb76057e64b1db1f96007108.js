19586e5f927b6b21b7eb3e00e972ef7c9725ea53
Aydin <aydindavut14@gmail.com>
2020-06-28 23:26:11 +0300

    Updates

---
+++ precache-manifest.f53c7f0ebb76057e64b1db1f96007108.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6f6fc69ed91acbd0713b5a5c6981b83",
+    "url": "/index.html"
+  },
+  {
+    "revision": "66f261f4fb941949b8ee",
+    "url": "/static/css/2.9d4f019c.chunk.css"
+  },
+  {
+    "revision": "cb504655254ad939daca",
+    "url": "/static/css/main.f7238401.chunk.css"
+  },
+  {
+    "revision": "66f261f4fb941949b8ee",
+    "url": "/static/js/2.29fb12e4.chunk.js"
+  },
+  {
+    "revision": "297829219adad9f6a5d583ce8f33a8b9",
+    "url": "/static/js/2.29fb12e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb504655254ad939daca",
+    "url": "/static/js/main.5a951f0d.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "c6d37b26297461a78d10f169dd5cf46d",
+    "url": "/static/media/Avenir-Roman-12.c6d37b26.ttf"
+  },
+  {
+    "revision": "8ea32e99070762d4e4803000c16f91d9",
+    "url": "/static/media/Valencia.8ea32e99.ttf"
+  }
+]);
