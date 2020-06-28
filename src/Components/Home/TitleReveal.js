7aeacaf0c2a4bebcbb113cbb45672569a251d0ee
Aydin <aydindavut14@gmail.com>
2020-06-28 22:42:11 +0300

    finished the project

---
+++ src/Components/Home/TitleReveal.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './NameStyle.css';
+
+// eslint-disable-next-line react/prop-types
+const TitleReveal = ({ text }) => (
+  // eslint-disable-next-line react/jsx-filename-extension
+  <>
+    <p className="Job">{text}</p>
+  </>
+);
+
+export default TitleReveal;
